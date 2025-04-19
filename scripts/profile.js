2709c29a0068a2017f6b41f6e9edfd3b03998dae
Zhe Wei Yeoh <yeoh.zhewei@yahoo.com>
2025-04-19 18:48:39 +0800

    Profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,27 @@
 import { handleInnerSidebarButtons } from "./sidebar.js";
 
 handleInnerSidebarButtons();
+handleHeaderImgHover();
+
+function handleHeaderImgHover() {
+    const imgOverlay = document.querySelector('.profile-header-img-overlay');
+    imgOverlay.addEventListener('mouseover', () => {
+        console.log("test");
+        imgOverlay.style.opacity = "1";
+
+    });
+    imgOverlay.addEventListener('mouseout', () => {
+        imgOverlay.style.opacity = "0";
+    });
+}
+
+function handleImgUpload() {
+    const img = document.querySelector('.profile-header-img');
+    const imgInput = document.getElementById('profileImage');
+
+    //Submit form when file is selected
+    imgInput.onchange = () => {
+        imgInput.submit();
+    }
+}
+
