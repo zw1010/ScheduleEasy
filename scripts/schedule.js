9f3dbfad00d2ccb8b185985b1037597cb8907c73
Zhe Wei Yeoh <yeoh.zhewei@yahoo.com>
2025-04-29 14:57:40 +0800

    Schedule page done

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -7,15 +7,31 @@ renderCalender(currentDate);
 handleCalenderButtons();
 
 function renderCalender(currentDate) {
+    const calenderContainer = document.querySelector('.js-schedule-calender-container');
     const monthPara = document.querySelector('.js-schedule-calender-date');
-    const calenderContainer = document.querySelector('.js-schedule-calender-grid-container');
 
     // Set calender month to today's month
     monthPara.innerHTML = currentDate.format('MMM YYYY');
 
+    // Calender days header
+    calenderContainer.innerHTML = `
+                                <div class="schedule-calender-header-grid-container">
+                                    <p class="font-small js-schedule-calender-header-day"><b>Sun</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Mon</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Tue</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Wed</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Thu</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Fri</b></p>
+                                    <p class="font-small js-schedule-calender-header-day"><b>Sat</b></p>
+                                </div>
+                                <div class="schedule-calender-grid-container js-schedule-calender-grid-container">
+                                </div>
+                                `;
+    const calenderGridContainer = document.querySelector('.js-schedule-calender-grid-container');
+
     //// Generate days of month (Grid items)
     // Create blank grid items
-    let html = "";
+    let gridHtml = "";
     const firstDay = currentDate.set('date', 1).format("ddd");
     const headerDayList = document.querySelectorAll('.js-schedule-calender-header-day');
     let flag = false;
@@ -24,7 +40,7 @@ function renderCalender(currentDate) {
             flag = true;
         }
         if (firstDay !== dayElement.innerText && flag===false) {
-            html += `
+            gridHtml += `
                     <div></div>
                     `;
         }
@@ -35,7 +51,7 @@ function renderCalender(currentDate) {
         const currentDayClass = currentDate.isSame(dayjs() , "day") && day === currentDate.date()
             ? "js-schedule-calender-date-container" : "";
  
-        html += `
+        gridHtml += `
                 <div class="schedule-calender-grid-item js-schedule-calender-grid-item">
                     <div class="schedule-calender-date ${currentDayClass}">
                         <p class=" font-small"><b>${day}</b></p>
@@ -45,7 +61,7 @@ function renderCalender(currentDate) {
                         data-day="${currentDate.set('date', day).format("dddd")}"></div>
                 </div>`;
     }
-    calenderContainer.innerHTML = html;
+    calenderGridContainer.innerHTML = gridHtml;
     
     handleCalenderDayHover();
 }
@@ -67,38 +83,61 @@ function handleCalenderButtons() {
 }
 
 function handleCalenderDayHover() {
-    const eventDetails = document.querySelectorAll('.js-schedule-overlay');
-    eventDetails.forEach((eventDetail) => {
-        eventDetail.addEventListener('mouseover', () => {
-            eventDetail.style.opacity = "1";
+    const dayList = document.querySelectorAll('.js-schedule-overlay');
+    dayList.forEach((day) => {
+        day.addEventListener('mouseover', () => {
+            day.style.opacity = "1";
     
         });
-        eventDetail.addEventListener('mouseout', () => {
-            eventDetail.style.opacity = "0";
+        day.addEventListener('mouseout', () => {
+            day.style.opacity = "0";
         });
 
-        eventDetail.addEventListener('click', () => {
-            renderCalenderDay(eventDetail.dataset.date, eventDetail.dataset.day);
+        day.addEventListener('click', () => {
+            renderCalenderDay(day);
         });
     });
 }
 
-function renderCalenderDay(date, day) {
-    ////Generate Day
-    const dayContainer = document.querySelector('.js-schedule-calender-day');
-    dayContainer.innerHTML = `
-                            <b>${date}</b> | ${day}
-                            `;
-    
-    //// Generate time
+function renderCalenderDay(dayElement) {
+    renderCalenderDayStatic(dayElement.dataset.date, dayElement.dataset.day);
+}
+
+function renderCalenderDayStatic(date, day) {
+    const calenderContainer = document.querySelector('.js-schedule-calender-container');
+
+    calenderContainer.innerHTML = `
+                               <div class="schedule-calender-day-container">
+                                    <div class="schedule-calender-day-header-container">
+                                        <p class="schedule-calender-day font-med js-schedule-calender-day"><b>${date}</b> | ${day}</p>
+                                        <button class="schedule-calender-day-cross-button js-schedule-calender-day-cross-button">&#10005;</button>
+                                    </div>
+                                    <div class="schedule-calender-time-event-container">
+                                        <div class="schedule-calender-time-grid js-schedule-calender-time-grid">
+                                        </div>
+                                        <div class="schedule-calender-event-grid js-schedule-calender-event-grid">
+                                        </div>
+                                    </div>
+                                </div>
+                                `;
+
+    // Handle cross button onclick
+    document.querySelector('.js-schedule-calender-day-cross-button')
+        .addEventListener('click', () => {
+            renderCalender(currentDate);
+        });
+
+    //// Generate Timestamps and Empty slots
     const timeGrid = document.querySelector('.js-schedule-calender-time-grid');
     const eventGrid = document.querySelector('.js-schedule-calender-event-grid');
     let html = "";
     let eventtHtml = "";
 
     eventtHtml += `
-                <div class="schedule-calender-day-event-first-slot"></div>
+                <div class="schedule-calender-day-event-first-slot 
+                    js-schedule-calender-day-event-slot-12-0-AM" data-time="12-0-AM"></div>
                 `;
+    
     // Generate AM
     for (let minute = 0; minute < 60; minute+=5) {
         if (minute === 0 || minute === 5)  {
@@ -114,7 +153,8 @@ function renderCalenderDay(date, day) {
         
         eventtHtml += minute !== 0 ? 
                     `
-                    <div class="schedule-calender-day-event-slot" data-time="12-${minute}-AM"></div>
+                    <div class="schedule-calender-day-event-slot 
+                        js-schedule-calender-day-event-slot-12-${minute}-AM" data-time="12-${minute}-AM"></div>
                     `: "";
     }
     for (let hour = 1; hour < 12; hour++) {
@@ -130,7 +170,8 @@ function renderCalenderDay(date, day) {
                         `;  
             }
             eventtHtml += `
-                        <div class="schedule-calender-day-event-slot" data-time="${hour}-${minute}-AM"></div>
+                        <div class="schedule-calender-day-event-slot 
+                            js-schedule-calender-day-event-slot-${hour}-${minute}-AM" data-time="${hour}-${minute}-AM"></div>
                         `;
         }
     }
@@ -148,7 +189,8 @@ function renderCalenderDay(date, day) {
                     `;  
         }
         eventtHtml += `
-                    <div class="schedule-calender-day-event-slot" data-time="12-${minute}-PM"></div>
+                    <div class="schedule-calender-day-event-slot 
+                        js-schedule-calender-day-event-slot-12-${minute}-PM" data-time="12-${minute}-PM"></div>
                     `;
     }
     for (let hour = 1; hour < 12; hour++) {
@@ -165,11 +207,44 @@ function renderCalenderDay(date, day) {
             }
             eventtHtml += (hour !== 11 || minute !== 55) ? 
                         `
-                        <div class="schedule-calender-day-event-slot" data-time="${hour}-${minute}-PM"></div>
+                        <div class="schedule-calender-day-event-slot 
+                            js-schedule-calender-day-event-slot-${hour}-${minute}-PM" data-time="${hour}-${minute}-PM"></div>
                         `: "";
         }
     }
 
     timeGrid.innerHTML = html;
     eventGrid.innerHTML = eventtHtml;
+}
+
+function renderCalenderDayDynamic() {
+    // If existing event
+    const existingHtml = `
+                        <div class="schedule-calender-day-event-slot schedule-calender-day-event">
+                            <p class="font-small">Dinner at Boon Lay</p>
+                        </div>
+                        `;
+
+    // If possible event
+    const possibleHtml = `
+                        <div class="schedule-calender-day-event-slot schedule-calender-day-possible-event">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <img src="images/profile.jpg" alt="">
+                            <!-- <p class="font-small">+...</p> -->
+                        </div>
+                        `;
 }
